c2170b479be4a2b4752fcbee808c31f8ca073964
Rebeca Martín <rebeca@numericco.com>
2022-10-24 08:36:48 +0200

    feat: add a more complete documentation for updateProvincePopulation endpoint using Swagger

--- app/src/Controller/UpdateProvincePopulationController.php
+++ app/src/Controller/UpdateProvincePopulationController.php
@@ -8,16 +8,33 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use OpenApi\Annotations as OA;
 
-#[Route('/api', name: 'api_')]
 class UpdateProvincePopulationController extends AbstractController
 {
-    #[Route(
-        '/province/{provinceId}',
-        name: 'updateProvincePopulation',
-        requirements: ['provinceId' => '\d+'],
-        methods: ['PUT']
-    )]
+    /**
+     * Update province population
+     *
+     * @Route("/api/province/{provinceId}", methods={"PUT"})
+     * @OA\Response(
+     *     response=200,
+     *     description="Success or Error",
+     * )
+     * @OA\Parameter(
+     *     name="provinceId",
+     *     in="path",
+     *     required=true,
+     *     description="The province ID",
+     *     @OA\Schema(type="number")
+     * )
+     * @OA\Parameter(
+     *     name="population",
+     *     in="query",
+     *     required=true,
+     *     description="The new population amount",
+     *     @OA\Schema(type="number")
+     * )
+     */
     public function updateProvincePopulation(
         Request $request,
         UpdateProvinceService $updateProvinceService
